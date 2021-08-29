60132c05226c6a33f88e8298c559ebe0b6a6865f
kptn-hoffman <kptn.hoffman@gmail.com>
2021-08-29 12:17:08 +0300

    added logging and new order endpoints for binance client

--- bin/client/exchange/BinanceClient.py
+++ bin/client/exchange/BinanceClient.py
@@ -20,6 +20,10 @@ class BinanceClient(Client):
     ALL_COINS_INFO_ENDPOINT = Endpoint('sapi/v1/capital/config/getall', signed=True)
     DAILY_SNAPSHOT_ENDPOINT = Endpoint('sapi/v1/accountSnapshot', signed=True)
 
+    # Spot trade endpoints
+    TEST_NEW_ORDER_ENDPOINT = Endpoint('api/v3/order/test', signed=True)
+    NEW_ORDER_ENDPOINT      = Endpoint('api/v3/order', signed=True)
+
     URL_TEMPLATE = "{}/{}{}"
 
     def __init__(self, api_key: str, api_secret: str) -> None:
@@ -30,7 +34,7 @@ class BinanceClient(Client):
 
     def get_system_status(self):
         system_status_url = self.__new_request_url(self.SYSTEM_STATUS_ENDPOINT, {})
-        logging.info('Getting system status:')
+        logging.info('Getting system status')
         return requests.get(url=system_status_url, headers=self.__get_default_headers()).json()
     
     def get_all_coins_info(self, recv_window: int = None):
@@ -38,12 +42,30 @@ class BinanceClient(Client):
         if recv_window:
             params['recvWindow'] = recv_window
         all_coins_info_url = self.__new_request_url(self.ALL_COINS_INFO_ENDPOINT, params)
+        logging.info('Getting all coins info')
         return requests.get(url=all_coins_info_url, headers=self.__get_default_headers()).json()
 
     def get_daily_account_snapshot(self, type: str = 'SPOT'):
         params = {'type': type}
         daily_snapshot_url = self.__new_request_url(self.DAILY_SNAPSHOT_ENDPOINT, params)
+        logging.info('Getting daily account snapshot')
         return requests.get(url=daily_snapshot_url, headers=self.__get_default_headers()).json()
+    
+    def get_most_recent_account_snapshot(self, type: str = 'SPOT'):
+        logging.info('Getting most recent account snapshot')
+        return self.get_daily_account_snapshot(type)['snapshotVos'][0]['data']['balances']
+
+    def get_asset_information(self, asset: str, type: str = 'SPOT'):
+        logging.info('Getting info on {}'.format(asset))
+        for assetDict in self.get_most_recent_account_snapshot(type):
+            if assetDict['asset'] == asset:
+                return assetDict
+        return {}
+
+    def test_new_order(self, symbol: str, side: str, quote_qty: int, type: str = 'MARKET'):
+        params = {'symbol': symbol, 'side': side, 'type': type, 'quantity': quote_qty}
+        test_new_order_url = self.__new_request_url(self.TEST_NEW_ORDER_ENDPOINT, params)
+        return requests.post(url=test_new_order_url, headers=self.__get_default_headers()).json()
 
     def __new_request_url(self, endpoint: Endpoint, params: dict) -> str:
         if endpoint.is_signed():
